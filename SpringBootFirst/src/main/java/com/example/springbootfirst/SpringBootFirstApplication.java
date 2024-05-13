753fb5a1dc375f5c92bd7ddff4be176ae9227d4b
DinilKumarasiri <d30.kumara@gmail.com>
2024-05-13 08:35:33 +0530

    get development branch

--- SpringBootFirst/src/main/java/com/example/springbootfirst/SpringBootFirstApplication.java
+++ SpringBootFirst/src/main/java/com/example/springbootfirst/SpringBootFirstApplication.java
@@ -2,13 +2,17 @@ package com.example.springbootfirst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringBootFirstApplication.class, args);
+        ApplicationContext context  = SpringApplication.run(SpringBootFirstApplication.class, args);
+
+        Alien obj = context.getBean(Alien.class);
+        obj.code();
         System.out.println("Hello World");
     }
 
